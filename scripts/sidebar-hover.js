ceee2e1ce4b1227cba355a4cc16f7c3cbbb801a5
Seva <beatskizaru3@gmail.com>
2025-08-01 15:06:42 +0300

    сделал сайдбар для пк с анимацией

---
+++ scripts/sidebar-hover.js
@@ -0,0 +1,10 @@
+// const sidebar = document.querySelector(".sidebar");
+// const main = document.querySelector(".main");
+
+// sidebar.addEventListener("mouseenter", ()=>{
+//     main.classList.add('expanded');
+// });
+
+// sidebar.addEventListener("mouseleave", ()=>{
+//     main.classList.remove('expanded');
+// });
